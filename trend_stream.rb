fa72e7317da68b61f0e69593390dc66b3ab94a45
Toshiaki Asai <qtoship@gmail.com>
2011-08-22 01:02:20 +0900

    first commit

---
+++ trend_stream.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Trend
+# 現在のTwitterのトレンドでStreaming APIでOR検索！
+
+require 'hpricot'
+
+Module.new do
+
+  plugin = Plugin::create(:trend_streaming)
+  main = Gtk::TimeLine.new()
+  main.force_retrieve_in_reply_to = false
+  queue_parse = Queue.new
+  queue_event = TimeLimitedQueue.new(4, 1){ |messages| Delayer.new{ main.add messages } }
+
+  Delayer.new {
+    service = Post.services.first
+    Plugin.call(:mui_tab_regist, main, 'Trend')
+
+    Thread.new{
+      loop{
+        sleep(3)
+        notice 'filter stream: connect'
+        begin
+          buzzword = open('http://buzztter.com/ja/', 'r'){ |io|
+            (Hpricot(io)/'div#buzzphrases'/'a').map{ |elm| elm.attributes['title'] }[0..7].join(',') }
+          if !buzzword or buzzword.empty?
+            sleep(60)
+          else
+            Plugin.call(:rewindstatus, "Buzzワード: #{buzzword}")
+            puts "Buzzワード: #{buzzword}"
+            timeout(60){
+              service.streaming(:filter_stream, :track => buzzword){ |x| queue_parse.push x } } end
+        rescue TimeoutError => e
+        rescue => e
+          warn e end
+        notice 'filter stream: disconnected' } }
+
+    Thread.new{
+      loop{
+        json = queue_parse.pop.strip
+        case json
+        when /^\{.*\}$/
+          messages = service.__send__(:parse_json, json, :streaming_status) rescue nil
+          if messages.is_a? Enumerable
+            messages.each{ |message|
+              queue_event.push message if message.is_a? Message } end end } }
+  }
+end
