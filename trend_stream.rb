b5d13f054b2abe8f3cec60753a6996691790ab09
Toshiaki Asai <qtoship@gmail.com>
2011-08-23 00:18:26 +0900

    optimize

--- trend_stream.rb
+++ trend_stream.rb
@@ -6,11 +6,11 @@ require 'hpricot'
 
 Module.new do
 
-  plugin = Plugin::create(:trend_streaming)
+  plugin = Plugin::create(:trend_trac_streaming)
   main = Gtk::TimeLine.new()
   main.force_retrieve_in_reply_to = false
-  queue_parse = Queue.new
-  queue_event = TimeLimitedQueue.new(4, 1){ |messages| Delayer.new{ main.add messages } }
+  queue_parse = SizedQueue.new(2)
+  queue_event = TimeLimitedQueue.new(4, 1){ |messages| Delayer.new(Delayer::LAST){ main.add messages } }
 
   Delayer.new {
     service = Post.services.first
@@ -43,6 +43,8 @@ Module.new do
           messages = service.__send__(:parse_json, json, :streaming_status) rescue nil
           if messages.is_a? Enumerable
             messages.each{ |message|
-              queue_event.push message if message.is_a? Message } end end } }
-  }
-end
+              queue_event.push message if message.is_a? Message
+            }
+          end
+        end } } } end
+
