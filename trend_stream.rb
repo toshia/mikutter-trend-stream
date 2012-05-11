58248b42f495d4914080c05943c743501abe750d
Toshiaki Asai <qtoship@gmail.com>
2012-05-11 15:21:45 +0900

    Streaming API同時接続本数制限対策のため、0.1.1のstreamingプラグインと連帯するようにした

--- trend_stream.rb
+++ trend_stream.rb
@@ -4,47 +4,39 @@
 
 require 'hpricot'
 
-Module.new do
+Plugin.create :trend_stream do
 
-  plugin = Plugin::create(:trend_trac_streaming)
   main = Gtk::TimeLine.new()
   main.force_retrieve_in_reply_to = false
-  queue_parse = SizedQueue.new(2)
-  queue_event = TimeLimitedQueue.new(4, 1){ |messages| Delayer.new(Delayer::LAST){ main.add messages } }
 
-  Delayer.new {
-    service = Post.services.first
-    Plugin.call(:mui_tab_regist, main, 'Trend')
+  Delayer.new{ Plugin.call(:mui_tab_regist, main, 'Trend') }
 
-    Thread.new{
-      loop{
-        sleep(3)
-        notice 'filter stream: connect'
-        begin
-          buzzword = open('http://buzztter.com/ja/', 'r'){ |io|
-            (Hpricot(io)/'div#buzzphrases'/'a').map{ |elm| elm.attributes['title'] }[0..7].join(',') }
-          if !buzzword or buzzword.empty?
-            sleep(60)
-          else
-            Plugin.call(:rewindstatus, "Buzzワード: #{buzzword}")
-            puts "Buzzワード: #{buzzword}"
-            timeout(60){
-              service.streaming(:filter_stream, :track => buzzword){ |x| queue_parse.push x } } end
-        rescue TimeoutError => e
-        rescue => e
-          warn e end
-        notice 'filter stream: disconnected' } }
+  # ばずったーからバズワードを取得して配列で返す。
+  # ==== Return
+  # ばずったーから取得したキーワードの配列
+  def rewind_buzzword
+    open('http://buzztter.com/ja/', 'r'){ |io|
+      @buzz = (Hpricot(io)/'div#buzzphrases'/'a').map{ |elm| elm.attributes['title'] }.sort.freeze } end
 
-    Thread.new{
-      loop{
-        json = queue_parse.pop.strip
-        case json
-        when /^\{.*\}$/
-          messages = service.__send__(:parse_json, json, :streaming_status) rescue nil
-          if messages.is_a? Enumerable
-            messages.each{ |message|
-              queue_event.push message if message.is_a? Message
-            }
-          end
-        end } } } end
+  def buzz
+    @buzz ||= rewind_buzzword
+  end
 
+  filter_filter_stream_track do |word|
+    if word.empty?
+      [buzz[0, 8].join(',')]
+    else
+      [word + "," + buzz[0, 8].join(',')] end end
+
+  on_period do
+    before = buzz
+    after = rewind_buzzword
+    if before != after
+      Plugin.call(:filter_stream_force_retry) end end
+
+  on_appear do |messages|
+    result = messages.select{ |message|
+      buzz.any?{ |b| message.to_s.include? b } }
+    main.add result if not result.empty? end
+
+end
